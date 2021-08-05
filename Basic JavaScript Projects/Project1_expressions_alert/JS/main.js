8034810f9b7ff1700675200f960ea1e0c5a12089
Jossheeh <83291710+Jossheeh@users.noreply.github.com>
2021-08-05 17:14:29 +0100

    first commit

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,38 @@
+
+    var B = "Hello, World!" //Creating variable B
+   window.alert(B); //Displaying Variable B in an alert window
+    document.write("hello, again<br>"); //Displaying text "Hello again"
+    var A = "This is a string<br>"; //Creating variable A
+    document.write(A); //Displaying variable A as text
+    var C = "John Said \"Hello Mary\" She replied \"Hi John\"<br>"; //creating variable C while using colons for quotation
+    document.write(C); //displaying variable C 
+    document.write ("I was today\'s years old when I"
+    +" learned how to concatenate"
+    +" using JavaScript<br>") //displaying text which has be concatenated
+
+
+    var d = "My Favourite animal is " 
+    + "a dog<br>"; 
+    document.write(d); //further concated text
+
+    var family = "Sheehans", dad = "Timmy", mom = "Majella", daughter1 = "Jade",
+    daughter2="Chelsea", son = "Josh<br>"; //assigning multiple variables
+
+    var family = family.fontcolor("black") //Changing font color for var family
+    var dad = dad.fontcolor("red") //Changing font color for var dad
+    var mom = mom.fontcolor("yellow") //Changing font color for var mom
+    var daughter1 = daughter1.fontcolor("orange") //Changing font color for var daughter1
+    var daughter2 = daughter2.fontcolor("green") //Changing font color for var daughter2
+    var son = son.fontcolor("blue") //Changing font color for var son
+
+    document.write(son); //displaying variable "son"
+
+
+
+
+    
+
+
+    document.write(3 * 3);
+
+ 
