e75a1bd5b55c8f07aec5b0ca87993703de82c51f
Jossheeh <83291710+Jossheeh@users.noreply.github.com>
2021-08-06 17:09:33 +0100

    Functions

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myfunction() { //Creating a function
+    Var = sentence = "I am learning"; //First line of text
+    sentence += " a lot from this book!"; //Second line of text which is concatenated
+    document.getElementById("concatenate").innerHTML = sentence; //using the document.getElementById function to display the text
+}
